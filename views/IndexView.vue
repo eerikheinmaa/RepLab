a0ea250b16a9fec31d8e975d813ae739393f980a
Joosep127 <48839946+Joosep127@users.noreply.github.com>
2025-02-20 15:15:13 +0200

    Broke everything

---
+++ views/IndexView.vue
@@ -0,0 +1,36 @@
+<template>
+
+  <head>
+    <meta charset="UTF-8">
+    <title>Web server</title>
+  </head>
+
+  <body>
+    <div class="Home_page">
+      <div class="Calender">
+
+      </div>
+      <div class="News">
+
+      </div>
+      <div class="Nutrition">
+
+      </div>
+      <div class="Start_workout">
+
+      </div>
+      <div class="Bar">
+
+      </div>
+
+      <!-- <div id="app"></div> 
+      <script type="module" src="/src/main.js"></script>-->
+    </div>
+  </body>
+
+  </html>
+</template>
+
+<script setup>
+import { onMounted } from 'vue'
+</script>
