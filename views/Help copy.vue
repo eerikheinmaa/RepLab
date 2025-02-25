a68b32386fc1cee0c6f2695a69e9a53d42b15108
Joosep127 <48839946+Joosep127@users.noreply.github.com>
2025-02-25 13:17:58 +0200

    Started porting Ats' work to git

---
+++ views/Help copy.vue
@@ -0,0 +1,8 @@
+<template>
+  <h1 style="color: white">HI</h1>
+  <router-link to="/about">Got to about</router-link>
+  <div class="Home_page">
+
+</template>
+
+<script setup></script>
---
+++ views/Help.vue
@@ -0,0 +1,61 @@
+<template>
+  <h1 style="color: white">HI</h1>
+  <router-link to="/help">Got to help</router-link>
+  <head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width">
+  <title>replit</title>
+  <link href="help.css" rel="stylesheet" type="text/css" />
+  <link href="https://fonts.googleapis.com/css2?family=Oswald:wght@400;700&family=Roboto:wght@400;700&display=swap" rel="stylesheet">
+</head>
+
+<body>
+  <div class="app">
+    <div class="calender">
+      <button class="calender-extencion" ><img src="assets/arrow.svg" class="arrow"></button>
+      <h3 class="calender-text">15.12</h3>
+      <h3 class="calender-text">14.12</h3>
+      <h3 class="calender-text">13.12</h3>
+      <h3 class="calender-text">12.12</h3>
+      <h3 class="calender-text today">11.12</h3>
+      <h3 class="calender-text">10.12</h3>
+      <h3 class="calender-text">09.12</h3>
+      <h3 class="calender-text">08.12</h3>
+      <h3 class="calender-text">07.12</h3>
+
+
+    </div>
+    <h1 class="title">IF YOU ARE HAVING A PROBLEM THEN CONTACT US</h1>
+    <div class="map">
+    </div>
+    <h2>Contacts:</h2>
+    <h3>Email: beast.app@gmail.com</h3>
+    <h3>Phone nr: +372 3490 2330</h3>
+
+
+
+    <div class="footer">
+      <div class="footer-pictures">
+        <button class="footer-buttons">
+          <img src="assets/profile.svg" class="profile">
+        </button>
+        <button class="footer-buttons">
+          <img src="assets/home1.svg" class="home">
+        </button>
+        <button class="footer-buttons">
+          <img src="assets/plans.svg" class="plans">
+        </button>
+        <button class="footer-buttons">
+          <img src="assets/help.svg" class="help">
+        </button>
+      </div>
+    </div>
+
+
+  </div>
+  <script src="script.js"></script>
+</body>
+
+</template>
+
+<script setup></script>
